d6ab6ce36d4317c1aa5b29cd0e43b2164dc926e7
kareza <kareza@qq.com>
2023-05-29 01:18:13 +0800

    将auth接口切换到默认url

--- wechat_power/urls.py
+++ wechat_power/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('auth', include("auth.urls")),
+    path('', include("auth.urls")),
     path('admin/', admin.site.urls),
 ]
