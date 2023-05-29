24095ef22cef51c32aac643fe365038d5fa8702b
kareza <kareza@qq.com>
2023-05-29 20:02:06 +0800

    修改根URL

--- wechat_power/urls.py
+++ wechat_power/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include("auth.urls")),
+    path('wechat_power/', include("auth.urls")),
     path('admin/', admin.site.urls),
 ]
